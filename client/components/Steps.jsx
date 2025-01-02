29adab2485025d904c245733b5b61e69cb6ea364
Christopher Hoffmann <cstephanhoffmann@gmail.com>
2025-01-02 16:50:34 -0500

    created Steps component, used to list out the steps on how to use TailorCV

---
+++ client/components/Steps.jsx
@@ -0,0 +1,39 @@
+import PropTypes from "prop-types";
+import "../styles/Steps.css";
+
+const Steps = ({ title, content, image, layout }) => {
+  return (
+    <div className={`step ${layout === "left" ? "step-left" : "step-right"}`}>
+      {layout === "left" && (
+        <>
+          <div className="step-content">
+            <h3>{title}</h3>
+            <p>{content}</p>
+          </div>
+          <div className="step-image">
+            <img src={image} alt={title} />
+          </div>
+        </>
+      )}
+      {layout === "right" && (
+        <>
+          <div className="step-image">
+            <img src={image} alt={title} />
+          </div>
+          <div className="step-content">
+            <h3>{title}</h3>
+            <p>{content}</p>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+Steps.propTypes = {
+  title: PropTypes.string.isRequired,
+  content: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  layout: PropTypes.oneOf(["left", "right"]).isRequired,
+};
+
+export default Steps;
