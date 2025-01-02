6fd6e9d3bcb8eeb6e831a6ad61a61f3853222842
Christopher Hoffmann <cstephanhoffmann@gmail.com>
2025-01-02 16:50:14 -0500

    created Footer component

---
+++ client/components/Footer.jsx
@@ -0,0 +1,16 @@
+import "../styles/Footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>© 2025 TailorCV. All Rights Reserved.</p>
+      <div className="socials">
+        <a href="#">GitHub</a>
+        <a href="#">LinkedIn</a>
+        <a href="#">Twitter</a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
