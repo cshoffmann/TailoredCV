19174e225e70366e073c54c4da340839148f406d
Christopher Hoffmann <cstephanhoffmann@gmail.com>
2025-01-02 16:50:02 -0500

    created Header component

---
+++ client/components/Header.jsx
@@ -0,0 +1,12 @@
+import "../styles/Header.css";
+
+const Header = () => {
+  return (
+    <header className="navbar">
+      <div className="logo">TailorCV</div>
+      <button className="use-button">Use TailorCV</button>
+    </header>
+  );
+};
+
+export default Header;
