b0083e61bb292709568a49008eb97bb4a31746ae
Christopher Hoffmann <cstephanhoffmann@gmail.com>
2025-01-02 22:09:42 -0500

    updated footer hover effect and links

--- client/components/Footer.jsx
+++ client/components/Footer.jsx
@@ -5,9 +5,8 @@ const Footer = () => {
     <footer className="footer">
       <p>© 2025 TailorCV. All Rights Reserved.</p>
       <div className="socials">
-        <a href="#">GitHub</a>
-        <a href="#">LinkedIn</a>
-        <a href="#">Twitter</a>
+        <a href="https://www.linkedin.com/in/christopher-hoffmann/">LinkedIn</a>
+        <a href="https://github.com/cshoffmann/TailoredCV">GitHub</a>
       </div>
     </footer>
   );
